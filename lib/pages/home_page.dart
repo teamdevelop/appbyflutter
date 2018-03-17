ce50ee27e1e5fdc3410fd30cdaa75c3a2ce0001e
teamdevelop <cycsdsd@126.com>
2018-03-17 18:14:58 +0800

    初始化第一篇

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+// import 'dart:io';
+import './other_page.dart';
+
+// final backgroundImage = new File('./asset/image/lake.jpg');
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("CYC"), backgroundColor: Colors.redAccent,),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text('CYC'),
+              accountEmail: new Text('exampor@126.com'),
+              currentAccountPicture: new GestureDetector(
+                onTap: () => print('current user'),
+                child: new CircleAvatar(
+                  backgroundImage: new NetworkImage('https://upload.jianshu.io/users/upload_avatars/7700793/dbcf94ba-9e63-4fcf-aa77-361644dd5a87?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'),
+                ),
+              ),
+              otherAccountsPictures: <Widget>[
+                new GestureDetector(
+                  onTap: () => print('other user'),
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage('https://upload.jianshu.io/users/upload_avatars/10878817/240ab127-e41b-496b-80d6-fc6c0c99f291?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'),
+                  ),
+                ),
+                new GestureDetector(
+                  onTap: () => print('other user'),
+                  child: new CircleAvatar(
+                    backgroundImage: new NetworkImage('https://upload.jianshu.io/users/upload_avatars/8346438/e3e45f12-b3c2-45a1-95ac-a608fa3b8960?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240'),
+                    ),
+                ),
+              ],
+              decoration: new BoxDecoration(
+                image: new DecorationImage(
+                  fit: BoxFit.fill,
+                  image: new NetworkImage('https://raw.githubusercontent.com/flutter/website/master/_includes/code/layout/lakes/images/lake.jpg')
+                )
+              ),
+            ),
+            new ListTile(
+              title: new Text('First Page'),
+              trailing: new Icon(Icons.arrow_upward),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new OtherPage('First Page')));
+              }
+            ),
+            new ListTile(
+              title: new Text('Second Page'),
+              trailing: new Icon(Icons.arrow_right),
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => new OtherPage('Second Page')));
+              } 
+            ),
+            new Divider(),
+            new ListTile(
+              title: new Text('Close'),
+              trailing: new Icon(Icons.cancel),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
+      ),
+      body: new Center(
+        child: new Text('HomePage',style: new TextStyle(fontSize: 35.0),),
+      ),
+    );
+  }
+}
